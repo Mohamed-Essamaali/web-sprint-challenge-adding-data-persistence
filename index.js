e1cfac0b9aba756be67f9b25e5ee58d7be00fa2a
Mohamed-Essamaali <Mohamed-Essamaali@lambdastudents.com>
2021-01-10 20:35:22 -0500

    installed dependencies and completed migrations and config db

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // start your server here
+const express= require('express')
+
+const server= express()
+
+const port = process.env.PORT || 5000
+
+server.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
+
